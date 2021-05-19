7f0c30f1bd1ab33fc8cb65c8a9857bc022cbc73a
Akhil mon <akhilmon@hotmail.com>
2021-05-19 19:57:54 +0530

    Form Color Changeddd

--- StudentMarkUpdation/Form1.Designer.cs
+++ StudentMarkUpdation/Form1.Designer.cs
@@ -136,6 +136,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
             this.ClientSize = new System.Drawing.Size(301, 263);
             this.Controls.Add(this.txbxAddress);
             this.Controls.Add(this.label5);
